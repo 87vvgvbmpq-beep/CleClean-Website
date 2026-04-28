3cc8a911e3b1de0158446f39951c15e5a8880b44
87vvgvbmpq-beep <87vvgvbmpq@privaterelay.appleid.com>
2026-04-28 01:02:31 -0400

    Remove quote form validation script

--- script.js
+++ script.js
@@ -17,20 +17,3 @@ comparisons.forEach((comparison) => {
 
   setPosition(range.value);
 });
-
-const quoteForm = document.querySelector(".quote-form");
-
-if (quoteForm) {
-  const interestFields = quoteForm.querySelectorAll('input[name="Interested In"]');
-
-  const validateInterest = () => {
-    const hasSelection = Array.from(interestFields).some((field) => field.checked);
-    interestFields[0].setCustomValidity(hasSelection ? "" : "Please choose at least one service.");
-  };
-
-  interestFields.forEach((field) => {
-    field.addEventListener("change", validateInterest);
-  });
-
-  quoteForm.addEventListener("submit", validateInterest);
-}
